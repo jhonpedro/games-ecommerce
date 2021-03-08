e88d1ba4cee5b33557506d360fb7a854ef3ea987
João Pedro <pedrobarros2010@gmail.com>
2021-03-08 18:37:32 -0300

    feat: displaying products to user

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import ProductsJSON from '../../services/products.json'
 
-import { LandingPageContainer, LandingHeader } from './styles'
+import { LandingPageContainer, LandingHeader, LandingContent } from './styles'
 import NavBar from '../../components/NavBar'
+import Card from '../../components/Card'
+
+interface ProductProps {
+	id: number
+	name: string
+	price: number
+	score: number
+	image: string
+}
 
 const Landing: React.FC = function () {
+	const products: Array<ProductProps> = ProductsJSON
+
 	return (
 		<LandingPageContainer>
 			<LandingHeader>
 				<NavBar />
 			</LandingHeader>
+			<LandingContent>
+				<h2>Produtos</h2>
+				<main className='products'>
+					{products.map((product) => {
+						return (
+							<Card
+								name={product.name}
+								price={product.price}
+								score={product.score}
+								image={product.image}
+							/>
+						)
+					})}
+				</main>
+			</LandingContent>
 		</LandingPageContainer>
 	)
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { CardContainer } from '../../components/Card/styles'
+
 export const LandingPageContainer = styled.div`
 	width: 100%;
 `
@@ -8,6 +10,29 @@ export const LandingHeader = styled.header`
 	width: 100%;
 	height: 5.5rem;
 	background-color: var(--color-primary);
+
 	display: flex;
 	align-items: center;
+`
+
+export const LandingContent = styled.div`
+	padding-bottom: 2rem;
+	width: 85%;
+
+	margin: 0 auto;
+
+	h2 {
+		font-size: 2rem;
+		padding: 1rem 0;
+	}
+
+	.products {
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+	}
+
+	${CardContainer} {
+		margin-top: 1rem;
+	}
 `
