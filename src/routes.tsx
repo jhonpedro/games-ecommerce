a31be46552901656308e7ac6cb8c7cf896c2657f
João Pedro <pedrobarros2010@gmail.com>
2021-03-08 20:29:34 -0300

    feat: creating cart page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import Cart from './pages/Cart/Index'
 import Landing from './pages/Landing'
 
 const Routes: React.FC = function () {
 	return (
 		<Switch>
-			<Route path='/'>
+			<Route path='/' exact>
 				<Landing />
+			</Route>
+			<Route path='/cart'>
+				<Cart />
 			</Route>
 		</Switch>
 	)
