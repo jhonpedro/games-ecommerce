7e2d5cb7b47949d9d9f64f21df4be752beec87be
João Pedro <pedrobarros2010@gmail.com>
2021-03-08 23:42:56 -0300

    refactor: adding basename in BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Routes from './routes'
 
 function App() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes />
 			<GlobalStyle />
 		</BrowserRouter>
